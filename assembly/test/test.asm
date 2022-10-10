549c4b7453875c242fabfff7ac4d56c5a7466c1e
patty <patrick.celee@gmail.com>
2022-10-11 01:54:09 +0800

    .

--- assembly/test/test.asm
+++ assembly/test/test.asm
@@ -1,23 +1,25 @@
+;movzx, movsx : 小的變數傳到大的裡面（WORD -> EAX）
+;xchg 交換
+;array + 
+
 include Irvine32.inc
 .model flat,stdcall
 .stack 4096
-ExitProcess PROTO, dwExitCode
+ExitProcess PROTO, dwExitCode:DWORD
 
 .data
-arr DWORD 1, 2, 3
-
+    arr BYTE 1, 2, 3, 4
 .code
 
+; 預設一個位置為8byte
 main PROC
-    mov EAX, arr
+    movzx EAX, arr
     call WriteInt
-    call clrf
-    xchg EAX, arr+1
+    call crlf
+    movzx EAX, arr+4
     call WriteInt
-    call clrf
-    xchg EAX, arr+2
-    call WriteInt
-    call clrf
+    call crlf
+    
 
     exit
 main ENDP
